82716629608db0048dd1f980c8380865741e149f
Abhishek Vashistha <vashistha.abhishek@gmail.com>
2019-04-30 15:42:22 +0530

    https://github.com/abvashis/KafkaCustomPartitioner

---
+++ readme.txt
@@ -0,0 +1,4 @@
+https://howtoprogram.xyz/2016/06/04/write-apache-kafka-custom-partitioner/
+
+
+kafka-topics --create --zookeeper localhost:2181  --replication-factor 1 --partitions 6 --topic UserMessageTopic
---
+++ src/main/java/com/kafka/custompartitioner/IUserService.java
@@ -0,0 +1,12 @@
+package com.kafka.custompartitioner;
+
+import java.util.List;
+
+public interface IUserService {
+
+  public Integer findUserId(String userName);
+  public List<String> findAllUsers();
+
+
+
+}
